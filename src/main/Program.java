cc03acd711b93ffcafcedf5763ba7df626252e89
Xu Jiyu <matexu97@gmail.com>
2022-06-06 19:47:20 +0200

    Gui Alap

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,7 +1,15 @@
 package main;
+
+import nezet.KincsKeresiGui;
+
 public class Program {
+
     public static void main(String[] args) {
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new KincsKeresiGui().setVisible(true);
+            }
+        });
     }
-    
+
 }
